d99c26a3cc2138b4fb6164a0c7e73df2b2ba7f6e
DennerOl <dener155@hotmail.com>
2023-05-14 16:15:33 -0300

    DataBase h2, Perfil de test JPA

--- src/main/java/com/educandoweb/curso/entities/User.java
+++ src/main/java/com/educandoweb/curso/entities/User.java
@@ -3,15 +3,25 @@ package com.educandoweb.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /* implements Serializable diz que o objeto pode ser 
  * transformado em cadeia de Dados para ser 
  * gravados em arquivos etc..
  */
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
